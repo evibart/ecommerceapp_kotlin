9d50f84d98fce7ea38d8e7721ee2b926408b80ba
evibart <eduvibart@hotmail.com>
2024-08-20 08:52:19 -0300

    first commit

---
+++ app/src/main/java/com/example/ecommercekotlin/activity/IntroActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ecommercekotlin.activity
+
+import android.content.Intent
+import android.os.Bundle
+import com.example.ecommercekotlin.MainActivity
+import com.example.ecommercekotlin.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.startBtn.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@IntroActivity, MainActivity::class.java
+                )
+            )
+        }
+    }
+}
